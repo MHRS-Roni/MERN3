0583996425ece86b6fb1610021cf948e0b07320d
MHRS-Roni <mdmonirhossain.program@gmail.com>
2023-03-23 18:20:15 +0600

    before going to namaz

--- _module01/assignment/main.js
+++ _module01/assignment/main.js
@@ -5,13 +5,21 @@
 // STEP 2 : Inside the function, create a for loop that iterates from 1 to 10.
 // STEP 3 : Inside the loop, multiply the current iteration number by the input "num" to generate the product.
 // STEP 4 : Log the product to the console in the format "num x iteration = product".
+// STEP 5 : Test the function with different input numbers to generate multiplication tables.
 
 //? Answer
 
-function multiplicationTable( num ){                 // STEP 1 : Done
-    for(let i = 1 ; i <= 10 ; i++){                  // Step 2 : Done
-        let product = num * i ;                      // Step 3 : Done
-        console.log(`${num} x ${i} = ${product}`);   // Step 4 : Done
+function multiplicationTable( num ){                     // STEP 1 : Done
+    console.log(`multificationTable\(${num}\)`);  //* loging title of the table in console 
+    for(let i = 1 ; i <= 10 ; i++){                      // Step 2 : Done
+        let product = num * i ;                          // Step 3 : Done
+        console.log(`${num}  x  ${i}  = ${product}`);    // Step 4 : Done
     }
+    console.log("");     //* loging an empty line to give line break between two multipication table in console
 }
-multiplicationTable(2);
+multiplicationTable(2);                                  // STEP 5 : Done
+multiplicationTable(3);                                  // STEP 5 : Done
+multiplicationTable(4);                                  // STEP 5 : Done
+multiplicationTable(9);                                  // STEP 5 : Done
+
+//! there are some 
