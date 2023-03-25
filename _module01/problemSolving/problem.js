4943ba3beb65d56aea603416f421c10615a9a727
MHRS-Roni <mdmonirhossain.program@gmail.com>
2023-03-26 01:30:40 +0600

    done

--- _module01/problemSolving/problem.js
+++ _module01/problemSolving/problem.js
@@ -16,42 +16,42 @@
         console.log(num);
     }
     
-    }//! end solution 01
+}//! end solution 01
     
-    {//* Solution 02: with while loop
-    console.log("challenge #1: with while loop")
+{//* Solution 02: with while loop
+console.log("challenge #1: with while loop")
     
-    let num = 1;
-    while(num <= 10){
-        console.log(num++);
-    }
+let num = 1;
+while(num <= 10){
+    console.log(num++);
+}
     
-    }//! end solution 02
+}//! end solution 02
     
     
-    {//* Solution 03: with do while loop
-    console.log("challenge #1: with do while loop")
+{//* Solution 03: with do while loop
+console.log("challenge #1: with do while loop")
     
-    let num = 1;
-    do{
-        console.log(num++)
-    }while(num <= 10)
+let num = 1;
+do{
+    console.log(num++)
+}while(num <= 10)
     
-    }//! end solution 03
+}//! end solution 03
     
-    //! -----------end of Coding challenge #1--------------------
+//! -----------end of Coding challenge #1--------------------
     
-    //TODO Coding challenge #2. Print the odd numbers less than 100
-    //? LOGIC 01: you can get all odd number by adding 2 with 1 for multiple time
+//TODO Coding challenge #2. Print the odd numbers less than 100
+//? LOGIC 01: you can get all odd number by adding 2 with 1 for multiple time
     
-    //? LOGIC 02: if a number devided by 2 remind 1 then the number is odd. You can get reminder with modulas(%) operator. You can incriment the number and check if number is odd,if it is odd you can log the number in console
+//? LOGIC 02: if a number devided by 2 remind 1 then the number is odd. You can get reminder with modulas(%) operator. You can incriment the number and check if number is odd,if it is odd you can log the number in console
     
-    //! ----------------Solution with logic 01-------------------
-    //STEP 01: create a variable num initialized with 1
-    //STEP 02: create a loop and iterate it as long as num < 100
-    //STEP 03: add 2 to the num in every iteration
+//! ----------------Solution with logic 01-------------------
+//STEP 01: create a variable num initialized with 1
+//STEP 02: create a loop and iterate it as long as num < 100
+//STEP 03: add 2 to the num in every iteration
     
-    {//* Solution 01: with for loop
+{//* Solution 01: with for loop
     
         
-    }
+}
